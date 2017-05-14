e043f4d8d4c0e5d74cbaa11a76580d87e555ec0f
yowkotsai <yowkotsai@gmail.com>
2017-05-14 19:41:15 +0800

    123

---
+++ gitattributes/removesecret.ps1
@@ -0,0 +1,4 @@
+﻿Param([string]$filePath)
+[xml]$Config = Get-Content -Path $filePath
+$Config.configuration.connectionStrings.add| foreach {$_.connectionString=$_.connectionString -replace '(data source|user id|password)=(.*?);','$1= - ;'}
+$Config.Save($filePath)
